7e6a5da989bd2eab5566bc3a834d5233bf1636c5
juanipisa <juanipi299@gmail.com>
2019-09-29 03:48:14 -0300

    SE AGREGO LA ALEATORIEDAD DE LAS CATEGORIAS

--- TP-ALGORITMOS/TP-ALGORITMOS.cpp
+++ TP-ALGORITMOS/TP-ALGORITMOS.cpp
@@ -1,11 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <thread>
+#include <chrono>
+
 using namespace std;
 
 struct pregunta {
 	char nombrePregunta[50];
 	bool respondida;
 	char respuesta[30];
+	pregunta * sgt;
 };
 
 struct categoria {
@@ -13,51 +19,103 @@ struct categoria {
 	pregunta arrayPreguntas[6];
 };
 
-struct jugador {
+struct nodoJugadores {
 	char nombre[20];
 	int puntos;
 	int turno;
-	pregunta pregJugador;
 };
+
 
 void juego(categoria arrayCategoria[]) {
 
+	int categoriaRandom = rand() % 7;
+	int i = 0;
+	char respuestaJugadorActual[30];
+	while (arrayCategoria[categoriaRandom].arrayPreguntas[i].respondida == true) {
+		i++;
+	}
+
+	cout << "Cargando categoria.";
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << " . ";
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << ". ";
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	cout << "Categoria: " << arrayCategoria[categoriaRandom].nombre<<endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	cout << arrayCategoria[categoriaRandom].arrayPreguntas[i].nombrePregunta << endl;
+	cout << "Respuesta: "; cin >> respuestaJugadorActual;
+
+	if (respuestaJugadorActual == arrayCategoria[categoriaRandom].arrayPreguntas[i].respuesta) {
+		cout << "Su respuesta es correcta!" << endl;
+	}
+
+	arrayCategoria[categoriaRandom].arrayPreguntas[i].respondida = true;
+
+
+}
+// Funcion donde se van a leer los Arhivos una vez que se inicie el programa
+void cargarJuego(categoria arrayCategoria[]) {
+
 	FILE* archivoCategorias = fopen("categorias.dat", "rb");
+	//FILE* archivoJugadores = fopen("jugadores.dat", "rb");
 
 	fread(&arrayCategoria[0], sizeof(categoria), 1, archivoCategorias);
-	cout << arrayCategoria[0].arrayPreguntas[0].nombrePregunta << endl;
-
+	
+	juego(arrayCategoria);
 }
-/*void push(nodo*& p, nodo* valor) {
-	nodo* aux = new nodo();
-	strcpy(aux->pregunta,valor->pregunta);
-	aux->sgt = p;
-	p = aux;
-}
-
-string pop(nodo*& p) {
-
-	string v = p->pregunta;
-	nodo* aux = p;
-	p = p->sgt;
-
-	delete aux;
-	return v;
-}*/
-int main()
-{
+// Funcion que se llama por primera vez para crear el juego
+void crearJuego() {
+	srand(time(0));
 	categoria arrayCategoria[7] = {};
-	strcpy(arrayCategoria[0].nombre, "ciencia");
+
+	strcpy(arrayCategoria[0].nombre, "Ciencia");
 	strcpy(arrayCategoria[0].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
 	arrayCategoria[0].arrayPreguntas[0].respondida = false;
-	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15 cm");
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[1].nombre, "Deportes");
+	strcpy(arrayCategoria[1].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[2].nombre, "Arte");
+	strcpy(arrayCategoria[2].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[3].nombre, "Historia");
+	strcpy(arrayCategoria[3].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[4].nombre, "Entretenimiento");
+	strcpy(arrayCategoria[4].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[5].nombre, "Geografia");
+	strcpy(arrayCategoria[5].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
+
+	strcpy(arrayCategoria[6].nombre, "Zaraza");
+	strcpy(arrayCategoria[6].arrayPreguntas[0].nombrePregunta, "Cual es el radio de tu ano?");
+	arrayCategoria[0].arrayPreguntas[0].respondida = false;
+	strcpy(arrayCategoria[0].arrayPreguntas[0].respuesta, "15");
 
 	FILE* archivoCategoria = fopen("categorias.dat", "wb");
+
 	for (int i = 0; i < 2; i++) {
 		fwrite(&arrayCategoria[i], sizeof(categoria), 1, archivoCategoria);
 	}
+	cargarJuego(arrayCategoria);
+}
 
-	juego(arrayCategoria);
-
+int main()
+{
+	crearJuego();
 }
 
